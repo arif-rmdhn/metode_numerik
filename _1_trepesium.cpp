842dbea305e363798ee9c8eae437e9914000997d
Arif Ramadhan <ariframadhan0859@gmail.com>
2024-10-23 16:19:45 +0700

    add file baru

---
+++ _1_trepesium.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+float f(float x){
+    return 1/(1+x);
+}
+
+int main(){
+    float sigma = 0, h,I,a,b,n,x;
+
+    printf("Masukkan niali a: ");
+    scanf("%f", &a);
+    printf("Masukkan niali b: ");
+    scanf("%f", &b);
+    printf("Masukkan niali iterasi: ");
+    scanf("%f", &n);
+
+    h=(b-a)/n;
+    x = a;
+    I = f(a) + f(b);
+
+    for (int i = 1; i < n; i++){
+        x += h;
+        sigma = sigma + 2*f(x);
+    }
+
+    I = (I+sigma)*h/2;
+    printf("Nilai Integrasi Numerik: %f", I);    
+}
