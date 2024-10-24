1702841e71249f44081db8d9f477f7affa21c5af
Arif Ramadhan <ariframadhan0859@gmail.com>
2024-10-24 08:16:02 +0700

    simpson1/3

--- _2_simpson13.cpp
+++ _2_simpson13.cpp
@@ -12,18 +12,23 @@ int main(){
     scanf("%f", &a);
     printf("Masukkan niali b: ");
     scanf("%f", &b);
-    printf("Masukkan niali iterasi: ");
+    printf("Masukkan jumlah iterasi: ");
     scanf("%f", &n);
 
     h=(b-a)/n;
     x = a;
     I = f(a) + f(b);
 
-    for (int i = 1; i < n; i++){
-        x += h;
-        sigma = sigma + 2*f(x);
-    }
+    for (int r = 1; r<=(n-1) ; r++){
+        x=x+h;
 
-    I = (I+sigma)*h/2;
-    printf("Nilai Integrasi Numerik: %f", I);    
+        if (r%2==1){
+            sigma =  sigma + 4*f(x);
+        } else{
+            sigma = sigma + 2*f(x);
+        }
+        
+    }
+    I = (I+sigma)*h/3;
+    printf("Nilai Integrasi Numerik metode simpson 1/3: %f", I);    
 }
